8b11b9d3765bd683094c3729828159ba9711b249
shlokexe <130241775+shlokexe@users.noreply.github.com>
2023-07-11 23:02:13 +0530

    device.mk

--- device.mk
+++ device.mk
@@ -6,3 +6,34 @@
 #
 
 LOCAL_PATH := device/samsung/m40
+
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+# TWRP specific build flags
+TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_EXTRA_LANGUAGES := true
+TW_INCLUDE_NTFS_3G := true
+TW_USE_TOOLBOX := true
+TW_INCLUDE_RESETPROP := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
+TW_DEFAULT_BRIGHTNESS := 1200
+TARGET_USES_MKE2FS := true
+TW_NO_LEGACY_PROPS := true
+TW_USE_NEW_MINADBD := true
+TW_NO_BIND_SYSTEM := true
+TW_NO_SCREEN_BLANK := true
+TW_EXCLUDE_APEX := true
+TW_FRAMERATE := 60
+
+
+TW_DEVICE_VERSION := m40
+
+
+TW_HAS_DOWNLOAD_MODE := true
+
+
+
+TWRP_INCLUDE_LOGCAT := true[/HEADING]
+[HEADING=2]TARGET_USES_LOGD := true
